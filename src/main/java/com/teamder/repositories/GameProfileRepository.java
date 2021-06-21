80e0f3bfa8d684bfc34cf1cba9f00057169d7380
Miguelm2i <miguelbvo59@gmail.com>
2021-06-21 17:26:17 +0200

    Récupération des profils

--- src/main/java/com/teamder/repositories/GameProfileRepository.java
+++ src/main/java/com/teamder/repositories/GameProfileRepository.java
@@ -9,6 +9,7 @@ import com.teamder.models.GameProfile;
 
 public interface GameProfileRepository extends JpaRepository<GameProfile, Long> {
 
+	// Récupère les profils, qui ne sont pas les siens, sur le même jeu, ET qu'il n'a pas déjà swiper, ET qui ne sont pas amis
 	@Query(value = "SELECT * FROM game_profile WHERE NOT id_GAMER = ?2 AND id_GAME = ?3 "
 			+ "AND id NOT IN (SELECT id_2_GAME_PROFILE FROM swipe WHERE id_GAME_PROFILE = ?1) "
 			+ "AND id_GAMER NOT IN (SELECT id_GAMER FROM friend WHERE id_2_GAMER = ?2) "
--- src/main/java/com/teamder/repositories/SwipeRepository.java
+++ src/main/java/com/teamder/repositories/SwipeRepository.java
@@ -1,9 +1,6 @@
 package com.teamder.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.teamder.models.Swipe;
 
